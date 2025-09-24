d8df746f59447dde5b88b464564182323df78743
schmitz-eline <eline.schmitz@student.hepl.be>
2025-09-24 16:39:12 +0200

    script008

--- starter/script008.js
+++ starter/script008.js
@@ -3,14 +3,26 @@
  */
 
 // 1. Initialisez une variable firstName à "John" et affichez sa valeur dans la console.
+const firstName = 'John';
+console.log('Prénom : ' + firstName);
 
 // 2. Initialisez une variable lastName à "Smith" et une variable age à 28.
+const lastName = 'Smith';
+console.log('Nom : ' + lastName);
+const age = 28;
+console.log('Âge : ' + age);
 
 // 3. Initialisez une variable fullAge à true et affichez-la.
+const fullAge = true;
+console.log('Âge complet : ' + fullAge);
 
 // 4. Déclarez une variable job et affichez sa valeur dans la console.
+let job;
+console.log('Job : ' + job);
 
 // 5. Affectez la valeur "Enseignant" à la variable job et affichez-la.
+job = 'Enseignant';
+console.log('Job : ' + job);
 
 // 6. Respectez les règles de nommage des variables : le nom ne peut pas
 // - commencer par un chiffre
