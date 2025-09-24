6e7238b680517645751476c9a0cc3d42e8ddfa8d
schmitz-eline <eline.schmitz@student.hepl.be>
2025-09-24 17:28:47 +0200

    script011

--- starter/script011.js
+++ starter/script011.js
@@ -4,12 +4,21 @@
 
 // Opérateurs multiples
 // 1. Utilisez les opérateurs +, *, et / pour effectuer une opération mathématique complexe. Affichez le résultat.
+const a = 3 + 5 * 9 / 4;
+console.log('a = 3 + 5 * 9 / 4 = ' + a);
 
 // Groupement
 // 2. Créez une expression mathématique qui nécessite le groupement des opérations. Utilisez des parenthèses pour clarifier l'ordre des opérations. Affichez le résultat.
+const b = (3 + 5) * 9 / 4;
+console.log('b = (3 + 5) * 9 / 4 = ' + b);
 
 // Affectations multiples
 // 3. Déclarez deux variables x et y. Utilisez des affectations multiples pour leur donner des valeurs. Par exemple, x = 5 et y = 10. Affichez les valeurs de x et y.
+const x = 5, y = 3;
+console.log('x = ' + x + ' et y = ' + y);
 
 // Autres opérateurs
 // 4. Utilisez un opérateur de comparaison (comme >, <, ==, etc.) pour comparer les valeurs de deux variables et affichez le résultat.
+console.log('x > y : ' + (x > y));
+console.log('x < y : ' + (x < y));
+console.log('x = y : ' + (x === y));
