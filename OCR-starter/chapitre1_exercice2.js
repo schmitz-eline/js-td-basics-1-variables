cffae94789991be36565371e1f5077284581688a
schmitz-eline <eline.schmitz@student.hepl.be>
2025-09-24 12:32:01 +0200

    ex2

--- OCR-starter/chapitre1_exercice2.js
+++ OCR-starter/chapitre1_exercice2.js
@@ -9,12 +9,25 @@ Affichez dans la console les opérations de base avec les nombres 6 et 3 :
 Avant d'exécuter le code, devinez à chaque fois le résultat de l'opération
 */
 
+const number1 = 6;
+const number2 = 3;
+
 // Somme
+const sum = number1 + number2;
+console.log('Somme : ' + sum);
 
 // Différence
+const difference = number1 - number2;
+console.log('Différence : ' + difference);
 
 // Multiplication
+const multiplication = number1 * number2;
+console.log('Multiplication : ' + multiplication);
 
 // Division
+const division = number1 / number2;
+console.log('Division : ' + division);
 
 // Modulo
+const modulo = number1 % number2;
+console.log('Modulo : ' + modulo);
